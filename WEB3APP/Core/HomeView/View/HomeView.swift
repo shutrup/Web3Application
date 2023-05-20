ac13bf51e8a635f7469f43a3b68f1533948be525
shutrup <magagaraev95@mail.ru>
2023-05-20 12:09:44 +0300

    create HomeView module, and create Wallet model

---
+++ WEB3APP/Core/HomeView/View/HomeView.swift
@@ -0,0 +1,42 @@
+//
+//  HomeView.swift
+//  WEB3APP
+//
+//  Created by Шарап Бамматов on 20.05.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Connect to:")
+                .font(.system(size: 17))
+                .fontWeight(.bold)
+            
+            Button {
+                
+            } label: {
+                HStack {
+                    Spacer()
+                    Text(Wallets.TrustWallet.name)
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                    Spacer()
+                }
+                .padding(.vertical, 15)
+                .background(Color.blue)
+                .cornerRadius(32)
+            }
+            .padding(.horizontal, 30)
+            .padding(.vertical, 24)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ WEB3APP/Core/HomeView/ViewModel/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  WEB3APP
+//
+//  Created by Шарап Бамматов on 20.05.2023.
+//
+
+import Foundation
+
+final class HomeViewModel: ObservableObject {
+    
+}
