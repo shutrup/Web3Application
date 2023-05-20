425c6c772230c8e05ae27ac66de9186f41a4f7b5
shutrup <magagaraev95@mail.ru>
2023-05-20 22:46:44 +0300

    add Metaball Animation

--- WEB3APP/Core/MainScreen/Views/MainScreenView.swift
+++ WEB3APP/Core/MainScreen/Views/MainScreenView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import MorphingShapes
 
 struct MainScreenView: View {
+    @State var dragOffset: CGSize = .zero
     
     var body: some View {
         ZStack {
@@ -33,15 +35,14 @@ struct MainScreenView: View {
                 .padding(.horizontal)
                 
                 ZStack {
-                    Circle()
-                        .frame(width: 230, height: 230)
-                        .foregroundColor(.accenttColor)
-                        .blur(radius: 20)
+                    ClubbedView()
+                        .blur(radius: 10)
+                        .zIndex(1)
                     
                     VStack(spacing: 18) {
                         Image("primary")
                             .resizable()
-                            .frame(width: 40, height: 44)
+                            .frame(width: 35, height: 44)
                             .foregroundColor(.white)
                             .shadow(color: Color.white.opacity(0.82), radius: 13, x: 0, y: 0)
                         
@@ -60,7 +61,7 @@ struct MainScreenView: View {
                                 .fontWeight(.semibold)
                         }
                     }
-                    .zIndex(1)
+                    .zIndex(2)
                 }
                 .padding(.top, 54)
                 
@@ -129,6 +130,45 @@ struct MainScreenView: View {
             }
         }
     }
+    
+    @ViewBuilder func ClubbedView() -> some View {
+        TimelineView(.animation(minimumInterval: 3.6, paused: false)) { _ in
+            Canvas { context, size in
+                context.addFilter(.alphaThreshold(min: 0.5, color: .accenttColor))
+                context.addFilter(.blur(radius: 30))
+                                  
+                context.drawLayer { ctx in
+                    for index in 1...15 {
+                        if let resolvedView = context.resolveSymbol(id: index) {
+                            ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: size.height / 2))
+                        }
+                    }
+                }
+            } symbols: {
+                ForEach(1...15, id: \.self) { i in
+                    let offset = CGSize(width: .random(in: -70...70), height: .random(in: -100...100))
+
+                    ClubbedRoundedRectangle(offset: offset)
+                        .tag(i)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder func Ball(offset: CGSize = .zero) -> some View {
+        Circle()
+            .fill(.white)
+            .frame(width: 150, height: 150)
+            .offset(offset)
+    }
+    
+    @ViewBuilder func ClubbedRoundedRectangle(offset: CGSize = .zero) -> some View {
+        RoundedRectangle(cornerRadius: 30, style: .continuous)
+            .fill(.white)
+            .frame(width: 120, height: 120)
+            .offset(offset)
+            .animation(.easeInOut(duration: 4), value: offset)
+    }
 }
 
 struct MainScreenView_Previews: PreviewProvider {
@@ -136,3 +176,22 @@ struct MainScreenView_Previews: PreviewProvider {
         MainScreenView()
     }
 }
+
+struct MyCustomShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.move(to: CGPoint(x: 0.52394*width, y: 0.00005*height))
+        path.addCurve(to: CGPoint(x: 0.87486*width, y: 0.24246*height), control1: CGPoint(x: 0.6775*width, y: 0.00289*height), control2: CGPoint(x: 0.78461*width, y: 0.12749*height))
+        path.addCurve(to: CGPoint(x: 0.9957*width, y: 0.60101*height), control1: CGPoint(x: 0.95852*width, y: 0.34903*height), control2: CGPoint(x: 1.01666*width, y: 0.47073*height))
+        path.addCurve(to: CGPoint(x: 0.76017*width, y: 0.95524*height), control1: CGPoint(x: 0.97249*width, y: 0.74527*height), control2: CGPoint(x: 0.89979*width, y: 0.88752*height))
+        path.addCurve(to: CGPoint(x: 0.29528*width, y: 0.94072*height), control1: CGPoint(x: 0.61569*width, y: 1.02532*height), control2: CGPoint(x: 0.44159*width, y: 1.00747*height))
+        path.addCurve(to: CGPoint(x: 0.00542*width, y: 0.61088*height), control1: CGPoint(x: 0.15076*width, y: 0.87478*height), control2: CGPoint(x: 0.03434*width, y: 0.75755*height))
+        path.addCurve(to: CGPoint(x: 0.15499*width, y: 0.22915*height), control1: CGPoint(x: -0.02219*width, y: 0.4708*height), control2: CGPoint(x: 0.05992*width, y: 0.34114*height))
+        path.addCurve(to: CGPoint(x: 0.52394*width, y: 0.00005*height), control1: CGPoint(x: 0.25124*width, y: 0.11576*height), control2: CGPoint(x: 0.36815*width, y: -0.00283*height))
+        path.closeSubpath()
+        return path
+    }
+}
+
