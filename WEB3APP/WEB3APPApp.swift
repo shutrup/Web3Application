2cecd99dec525776b2df5d7836017ae6dcbc350a
shutrup <magagaraev95@mail.ru>
2023-05-20 11:00:22 +0300

    Initial Commit

---
+++ WEB3APP/WEB3APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WEB3APPApp.swift
+//  WEB3APP
+//
+//  Created by Шарап Бамматов on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WEB3APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
